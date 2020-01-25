fa6dad98c05bbfd9377a40e2b9f27d07b3d7bb12
Alexander Linder <alexander-linder@gmx.de>
2020-01-25 02:07:57 +0100

    Implement rump plugin

--- Plugin/src/main/java/de/seine_eloquenz/spigot_pacman_plugin/SpigotPacman.java
+++ Plugin/src/main/java/de/seine_eloquenz/spigot_pacman_plugin/SpigotPacman.java
@@ -1,17 +1,23 @@
 package de.seine_eloquenz.spigot_pacman_plugin;
 
 import de.seine_eloquenz.lbcfs.LbcfsPlugin;
+import de.seine_eloquenz.spigot_pacman_libs.Constants;
+import org.bukkit.plugin.java.annotation.dependency.Dependency;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 import org.jgroups.JChannel;
 
+@Plugin(name = "SPM", version = "1.0")
+@Author("Seine_Eloquenz")
+@Dependency("LBCFS")
 public class SpigotPacman extends LbcfsPlugin {
 
-    private JChannel channel;
+    private final JChannel channel;
 
     public SpigotPacman() throws Exception {
         this.channel = new JChannel();
-        this.channel.connect("spigotPacman");
         this.channel.name("server");
-        this.channel.setReceiver(new ShutdownListener());
+        this.channel.connect(Constants.CHANNEL_NAME);
     }
 
     public String getChatPrefix() {
