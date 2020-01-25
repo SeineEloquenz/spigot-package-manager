4a55ef2e03b85bb10260a52084e3dba3040bee05
Alexander Linder <alexander-linder@gmx.de>
2020-01-25 02:29:12 +0100

    Cleanup Command.java

--- Service/src/main/java/de/seine_eloquenz/spigot_pacman_service/cmd/Command.java
+++ Service/src/main/java/de/seine_eloquenz/spigot_pacman_service/cmd/Command.java
@@ -2,11 +2,9 @@ package de.seine_eloquenz.spigot_pacman_service.cmd;
 
 import de.seine_eloquenz.spigot_pacman_service.SpigotPacman;
 
-import java.io.File;
-
 public abstract class Command {
 
-    private final SpigotPacman pacman;
+    final SpigotPacman pacman;
 
     public Command(SpigotPacman pacman) {
         this.pacman = pacman;
@@ -19,14 +17,4 @@ public abstract class Command {
     }
 
     abstract void executeLogic(String... args);
-
-    protected void awaitSpigotShutdown() {
-        File spigotJar = new File("./spigot.jar");
-        while (!spigotJar.canWrite()) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ignored) {
-            }
-        }
-    }
 }
