c4724a6cdbe49ef979839206c7aca65a86cc69e0
Alexander Linder <alexander-linder@gmx.de>
2020-05-25 20:27:12 +0200

    Update dependency installation script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,3 +2,6 @@
 git clone https://github.com/SeineEloquenz/lbcfs.git
 cd lbcfs/ || exit
 mvn install
+git clone https://github.com/SeineEloquenz/lbcfs-plugin-annotations.git
+cd lbcfs-plugin-annotations/ || exit
+mvn install
