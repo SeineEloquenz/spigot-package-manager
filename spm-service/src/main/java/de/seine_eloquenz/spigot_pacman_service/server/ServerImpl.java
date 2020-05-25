ea8baa626d4b8bb6d00bf17e9ddbec862acd8e90
Alexander Linder <alexander-linder@gmx.de>
2020-05-25 23:06:58 +0200

    Add nogui flag to processBuilder

--- spm-service/src/main/java/de/seine_eloquenz/spigot_pacman_service/server/ServerImpl.java
+++ spm-service/src/main/java/de/seine_eloquenz/spigot_pacman_service/server/ServerImpl.java
@@ -32,7 +32,7 @@ public class ServerImpl implements Server {
     public ServerImpl(String... commandLineParams) {
         this.commandLineParams = commandLineParams;
         processBuilder = new ProcessBuilder();
-        processBuilder.command(Stream.concat(Stream.of("java", "-jar", "spigot.jar"), Arrays.stream(commandLineParams))
+        processBuilder.command(Stream.concat(Stream.of("java", "-jar", "spigot.jar", "--nogui"), Arrays.stream(commandLineParams))
                         .collect(Collectors.toList()));
     }
 
