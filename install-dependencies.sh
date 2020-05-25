806faae89dc285ed536d02ded5db7b135b32aef0
Alexander Linder <alexander-linder@gmx.de>
2020-05-25 21:32:35 +0200

    Fix it again, maybe it works now

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/SeineEloquenz/lbcfs-plugin-annotations.git
 cd lbcfs-plugin-annotations/ || exit
 mvn install
+cd ..
 git clone https://github.com/SeineEloquenz/lbcfs.git
 cd lbcfs/ || exit
 mvn install
